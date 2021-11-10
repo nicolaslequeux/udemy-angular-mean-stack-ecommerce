41685e617d1e8208c0b7009223743d9cd3d47f86
Nicolas Lequeux <contact@nicolaslequeux.com>
2021-11-10 13:57:23 +0100

    heroku backend

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # udemy-angular-mean-stack-ecommerce
 
-NodeJS - ExpressJS - Angular - NX Monorepo - PrimeNG - MongoDB
+NodeJS - ExpressJS - Angular - NX Monorepo - PrimeNG - MongoDB - Heroku
 
 eshop-backend: npm run start
+
 eshop-front/shop : nx serve ngshop
-eshop-front/admin : nx serve admin
+
+eshop-front/admin : nx serve admin --port=4201
 
 fake user : nicolas@gmail.com + 123456
 
--- eshop-backend/helpers/jwt.js
+++ eshop-backend/helpers/jwt.js
@@ -9,23 +9,25 @@ function authJwt() {
         isRevoked: isRevoked
     }).unless({
         path: [
-            // { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
-            // { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
-            // { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
-            // { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            // `${api}/users/login`,
-            // `${api}/users/register`
-            { url: /(.*)/ }, // Unprotect every routes !!
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['POST', 'OPTIONS'] },
+            `${api}/users/login`,
+            `${api}/users/register`
         ]
     });
 }
 
 async function isRevoked(req, payload, done) {
-    if (!payload.isAdmin) {
-        done(null, true);
+    console.log(req.url);
+    console.log(req.method);
+    if (payload.isAdmin) {
+        done();
+    } else if (req.url.include('orders')) {
     }
 
-    done();
+    done(null, true);
 }
 
 module.exports = authJwt;
